1343406818f5aa84734c766648cca8240863d258
BumindaNawagamuwa <buminda.nawagamuwa@health.telstra.com>
2022-10-12 22:18:00 +1000

    #Update landing page

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -13,8 +13,8 @@ export class Home extends Component {
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
           <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-                <li><a href='https://www.hl7.org/fhir/smart-app-launch/'>Smart On Fhir</a> Smart On Fhir</li>
-                <li><a href='https://docs.smarthealthit.org/'>Smart App Launch</a> Smart App Launch</li>
+          <li><a href='https://www.hl7.org/fhir/smart-app-launch/'>Smart On Fhir</a> Smart On Fhir</li>
+          <li><a href='https://docs.smarthealthit.org/'>Smart App Launch</a> Smart App Launch</li>
         </ul>
         <p>To help you get started, we have also set up:</p>
         <ul>
